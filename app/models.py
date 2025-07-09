bf2bd60bb5c748926c6bb817f2b828d5f83adb6a
lora-rakesh <garbhanirakeshkumar@gmail.com>
2025-07-09 11:55:55 +0530

    update

--- app/models.py
+++ app/models.py
@@ -5,26 +5,39 @@ class Company(models.Model):
 
     def __str__(self):
         return self.name
-# models.py
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
 
 class EmployeeManager(BaseUserManager):
     def create_user(self, employee_id, password=None, **extra_fields):
         if not employee_id:
             raise ValueError('Employee ID is required')
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
         user = self.model(employee_id=employee_id, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-class Employee(AbstractBaseUser):
+    def create_superuser(self, employee_id, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(employee_id, password, **extra_fields)
+
+class Employee(AbstractBaseUser, PermissionsMixin):
     employee_id = models.CharField(max_length=20, unique=True)
     name = models.CharField(max_length=100)
     is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)  # required for admin access
 
     USERNAME_FIELD = 'employee_id'
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = ['name']
 
     objects = EmployeeManager()
 
